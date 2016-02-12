b330daf262802bfd79e8ec949e9c344c721e87ae
Grayson Hicks <graysonhicks@gmail.com>
2016-02-12 14:08:20 -0500

    added number input functionality, working on AC button and operators

--- js/main.js
+++ js/main.js
@@ -1,26 +1,11 @@
 // Calculator Object?
 
-/*var calculator = {
-    sum: 0,
-    add: function(value) {
-        this.sum += value;
-    },
-    minus: function(value) {
-        this.sum -= value;
-    },
-    clear: function() {
-        this.sum= 0;
-    },
-    equals: function() {
-        return this.sum;
-    }
-};*/
-
-
+// Use switch and cases?
+//is number function return Number(n) === n
 //As function
 
-var calculator = function(val){
-  this.sum = val;
+var calculator = function(){
+  this.sum = 0;
   this.add = function(value) {
     this.sum += value;
     return this;
@@ -46,13 +31,58 @@ var calculator = function(val){
   return this;
 };
 
-console.log(calculator(6).add(6).equals());
+console.log(calculator(0).add(90).multiply(3).equals());
 
 // Long else if statements with different button names? with operators inside?
 
-var displayArea = document.querySelector(".display");
-var runningValue = displayArea.val();
-var keyPressed =
- if (keyPressed = //a number//) {
+//BUTTONS
+
+// GET BUTTON
+var numberButtons = document.getElementsByClassName("numbers");
+var operatorButtons = document.getElementsByClassName("operators");
+
+function numberButtonHandler(button){
+  // Get value from button clicked
+    var value = this.attributes.value.nodeValue;
+
+  displayNumber(value);
+  console.log("clicked!");
+}
+
+// CLICK LISTENERS
+for (i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener("click", numberButtonHandler);
+}
+
+for (i = 0; i < operatorButtons.length; i++) {
+  operatorButtons[i].addEventListener("click", operatorButtonHandler);
+}
+
+
+
+// Display value of number clicked in display div
+function displayNumber(value){
+  console.log("display!");
+  console.log(value);
+  var displayArea = document.getElementById("display-span");
+  displayedNumber = displayArea.innerHTML;
+  console.log(displayArea.innerHTML);
+  //if AC button is clicked
+  if (value === "ac") {
+    displayedNumber = "0";
+  } else if (displayedNumber === "0"){ // if 0 is already displayed, then set to number clicked
+    displayedNumber = value;
+  }   //otherwise, add the clicked number next in line to exiting number in display
+    else {
+      displayedNumber += value;
+    }
+}
+
+function operatorButtonHandler(){
+
+}
+//var runningValue = displayArea.val();
+//var keyPressed =
+ //if (keyPressed = //a number//) {
    //
- }
+// }
