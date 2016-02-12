e77356072e0243f6ec5cafd9840b324d06752940
Grayson Hicks <graysonhicks@gmail.com>
2016-02-12 16:05:55 -0500

    working on operators, equal sign and proper answer displaying

--- js/main.js
+++ js/main.js
@@ -31,23 +31,19 @@ var calculator = function(){
   return this;
 };
 
-console.log(calculator(0).add(90).multiply(3).equals());
+
 
 // Long else if statements with different button names? with operators inside?
 
 //BUTTONS
 
 // GET BUTTON
-var numberButtons = document.getElementsByClassName("numbers");
-var operatorButtons = document.getElementsByClassName("operators");
-
-function numberButtonHandler(button){
-  // Get value from button clicked
-    var value = this.attributes.value.nodeValue;
-
-  displayNumber(value);
-  console.log("clicked!");
-}
+var numberButtons = document.getElementsByClassName("numbers"); // Number buttons
+var operatorButtons = document.getElementsByClassName("operators"); //Operator buttons
+var equalButton = document.getElementById("equal"); // Equal button
+var operatorPushed = false; //flag to determine if operator has just been pushed
+var numberPushed = false; //flag to determine if number has just been pushed
+var equation = ""; //var to hold for building equation
 
 // CLICK LISTENERS
 for (i = 0; i < numberButtons.length; i++) {
@@ -58,31 +54,86 @@ for (i = 0; i < operatorButtons.length; i++) {
   operatorButtons[i].addEventListener("click", operatorButtonHandler);
 }
 
+equalButton.addEventListener("click", equalButtonHandler);
 
+// CLICK HANDLERS
+  // NUMBER BUTTON HANDLER
+function numberButtonHandler(button){
+  // Get value from button clicked
+    var value = this.attributes.value.nodeValue;
+    var isAcButton;
 
-// Display value of number clicked in display div
-function displayNumber(value){
-  console.log("display!");
-  console.log(value);
-  var displayArea = document.getElementById("display-span");
-  displayedNumber = displayArea.innerHTML;
-  console.log(displayArea.innerHTML);
-  //if AC button is clicked
-  if (value === "ac") {
-    displayedNumber = "0";
-  } else if (displayedNumber === "0"){ // if 0 is already displayed, then set to number clicked
-    displayedNumber = value;
-  }   //otherwise, add the clicked number next in line to exiting number in display
-    else {
-      displayedNumber += value;
+    if (this.attributes.value.nodeValue == "ac"){
+      isAcButton = true;
+      displayNumber(value, isAcButton);
+    } else {
+      isAcButton = false;
+      displayNumber(value, isAcButton);
+      }
+
+}
+
+  // OPERATOR BUTTON HANDLER
+function operatorButtonHandler(button){
+  var operand = document.getElementById("display");
+  operand = operand.value;
+  var value = this.attributes.value.nodeValue;
+
+  if (numberPushed === false) {
+    alert("Please hit a number first!");
+  } else {
+
+    switch (value) {
+    case "*":
+              console.log(equation);
+              console.log(operand);
+              console.log(value);
+              equation = equation * operand;
+              console.log(equation);
+              break;
+    default:
+      console.log(".");
     }
+  operatorPushed = true; // Sets operator flag
+  }
 }
 
-function operatorButtonHandler(){
+  // EQUAL BUTTON HANDLER
+function equalButtonHandler(button) {
+  var displayedNumber = document.getElementById("display");
 
+  if (numberPushed === false) {
+    alert("Please do some math first!");
+  } else {
+      displayedNumber.value += equation;
+  }
 }
-//var runningValue = displayArea.val();
-//var keyPressed =
- //if (keyPressed = //a number//) {
-   //
-// }
+
+  // DISPLAY FUNCTION
+
+function displayNumber(value, isAcButton){
+  var displayedNumber = document.getElementById("display");
+  //if AC button is clicked
+  if (isAcButton === true) {        //if clear button pushed
+    displayedNumber.value = "0";    // set displayedNumber back to "0"
+    equation = "";                //reset equation
+    operatorPushed = false;       // set flag back to flase
+  } else if (operatorPushed === true) { //if an operator has just been pushed, set to new number pushed
+    displayedNumber.value = value;
+    equation += value;                  //add pushed numbers to equation
+    operatorPushed = false;             // set flag back to false
+    numberPushed = true;
+    } else if (displayedNumber.value == "0"){ // if 0 is already displayed, then set to number clicked
+      displayedNumber.value = value;
+      equation += value;
+      console.log(equation);
+      operatorPushed = false;
+      numberPushed = true;
+    }  else {    //otherwise, add the clicked number next in line to exiting number in display
+        displayedNumber.value += value;
+        equation += value;
+        console.log(equation);
+        operatorPushed = false;
+        numberPushed = true;
+      }
+}
